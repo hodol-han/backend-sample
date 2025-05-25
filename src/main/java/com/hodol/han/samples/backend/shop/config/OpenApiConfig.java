fe3fa3b1551153c37c20ed68b338e9094327fc60
Hodol Han <bab6ting@gmail.com>
2025-05-25 21:17:25 +0900

    fix(openapi): Restrict to 'dev' profile for development

--- src/main/java/com/hodol/han/samples/backend/shop/config/OpenApiConfig.java
+++ src/main/java/com/hodol/han/samples/backend/shop/config/OpenApiConfig.java
@@ -6,8 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+// Restricting OpenAPI configuration to the 'dev' profile to ensure it is only enabled in
+// development environments.
+@Profile("dev")
 public class OpenApiConfig {
 
   @Bean
